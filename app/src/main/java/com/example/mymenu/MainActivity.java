74e03562b1934cdf3100538cdb924cc585868ff7
RomanGoltsov <romer533@mail.ru>
2021-03-03 11:10:21 +0600

    Completed Task

--- app/src/main/java/com/example/mymenu/MainActivity.java
+++ app/src/main/java/com/example/mymenu/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mymenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView headerView = (TextView) findViewById(R.id.selectedMenuItem);
+        switch (id) {
+            case R.id.action_settings:
+                headerView.setText("Настройки");
+                return true;
+            case R.id.open_settings:
+                headerView.setText("Открыть");
+                return true;
+            case R.id.save_settings:
+                headerView.setText("Сохранить");
+                return true;
+        }
+        //headerView.setText(item.getTitle());
+        return super.onOptionsItemSelected(item);
+    }
+
 }
